6c16f7fe60498cce7c98c3709980b85f5ba5773e
Peter Higgins <pah@cenabo.com>
2018-04-12 11:39:32 +0100

    Add image upload feature

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://pah:pah123@ds163494.mlab.com:63494/blog_dev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAIJX2GHTMC56YSRZA',
+  secretAccessKey: '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'
 };
